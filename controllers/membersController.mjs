9329f52b728c6698340225c7523cf130ac004327
hari-iocod <harikrishnan@iocod.com>
2024-01-28 00:07:41 +0530

    code refactoring

--- controllers/membersController.mjs
+++ controllers/membersController.mjs
@@ -1,49 +1,53 @@
 import Members from '../models/Members.mjs'
 
+const handleResponse = (res, status, data) => {
+    res.status(200).json({ success: status === 200, data });
+}
+
+const handleError = (res, error) => {
+    res.status(500).send(error);
+};
+
 export const addMember = async (req, res) => {
     const { firstName, lastName, age, gender, contact, dateOfJoining, dateOfBirth } = req.body;
 
     try {
         const member = new Members({ firstName, lastName, age, gender, contact, dateOfJoining, dateOfBirth })
         await member.save();
-        res.status(200).json({ success: true, message: 'Member added successfully' });
-
+        handleResponse(res, 200, 'Member added successfully')
     } catch (error) {
-        res.status(500).send(error);
+        handleError(res, error)
     }
 }
 
 export const allMembers = async (req, res) => {
     try {
         const members = await Members.find();
-        res.status(200).json({ success: true,  data: members});
-
+        handleResponse(res, 200, members)
     } catch (error) {
-        res.status(500).send(error);
+        handleError(res, error)
     }
 }
 
 export const deleteOneMember = async (req, res) => {
     const documentId = req.params.id
     try {
-       await Members.deleteOne({ _id: documentId}) ;
-        res.status(200).json({ success: true, message: 'Member Deleted successfully'});
+        await Members.deleteOne({ _id: documentId });
+        handleResponse(res, 200, 'Member Deleted successfully')
 
     } catch (error) {
-        res.status(500).send(error);
+        handleError(res, error)
     }
-
 }
 
 export const updateMember = async (req, res) => {
     const documentId = req.params.id
     try {
-       await Members.updateOne({ _id: documentId}, req.body)
-        res.status(200).json({ success: true, message: 'Member Updated successfully'});
+        await Members.updateOne({ _id: documentId }, req.body)
+        handleResponse(res, 200, 'Member Updated successfully')
 
     } catch (error) {
-        res.status(500).send(error);
+        handleError(res, error)
     }
-
 }
 
